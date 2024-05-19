8d1e0163413991d16d988aedadc01d93c2f894a9
Vladyslav Aleksiienkov <73491176+Vladhadka@users.noreply.github.com>
2024-05-19 18:36:02 +0300

    Скільки тобі років final

--- homework2.js
+++ homework2.js
@@ -7,8 +7,11 @@ prompt.get(['age'], function (err,result) {
 
 if (result.age % 10 === 1 && result.age<=9)
     {console.log('Тобі' + " " + result.age + " " + "рік");}
-    else if(result.age % 20 >= 2 && result.age % 20 <=4)
+else if(result.age  >=11 && result.age <=19)
+    {console.log('Тобі' + " " + result.age + " " + "років");}
+    else if(result.age % 10 >= 2 && result.age % 10 <=4)
         {console.log('Тобі' + " " + result.age + " " + "роки");}
+   
     else 
     {console.log('Тобі' + " " + result.age + " " + "років");}
 

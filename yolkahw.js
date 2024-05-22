ae6c1f26cd3a94c8c650c14c28cee726d255696c
Vladyslav Aleksiienkov <73491176+Vladhadka@users.noreply.github.com>
2024-05-22 23:58:57 +0300

    Homework 5.1

---
+++ yolkahw.js
@@ -0,0 +1,28 @@
+function triangle(h, Symbol) {
+    let a = '';
+    for (let i = 0; i < h; i++) {
+        
+        for (j = 0 ; j<=i ;j++){
+            a += Symbol;}
+            a += '\n'}
+           return a;}
+       
+    
+    
+    console.log(triangle(5, '*'));
+
+
+
+
+function tr(h,Symbol){
+    let a = '';
+    
+    for (let i = 0; i < h; i++){
+    console.log(a);
+   a = a + Symbol;
+  
+   
+
+}}
+
+console.log(tr(8,'*'));

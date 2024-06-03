651eb69778b49c7a1889d6e71fe32aadfe9eda5c
Vladyslav Aleksiienkov <73491176+Vladhadka@users.noreply.github.com>
2024-06-03 23:51:40 +0300

    ObjectSorceserHarry

--- object.js
+++ object.js
@@ -24,27 +24,3 @@ sorceserHarry.getInfo();
 
 
 
-// Створення об'єкта з трьома властивостями
-// let myObject = {
-//     name: 'Alice',
-//     age: 30,
-//     city: 'Wonderland',
-//     getInfo: function() {
-//         let info = '';
-//         for (let key in this) {
-//             if (typeof this[key] !== 'function') {
-//                 info += key + ': ' + this[key] + '\n';
-//             }
-//         }
-//         console.log(info);
-//     }
-// };
-
-// // Виклик методу getInfo для виведення інформації про об'єкт
-// myObject.getInfo();
-
-// // Додавання нової властивості до об'єкта
-// myObject.job = 'Adventurer';
-
-// // Виклик методу getInfo після додавання нової властивості
-// myObject.getInfo();
